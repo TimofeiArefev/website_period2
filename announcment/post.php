1619a8c54c943eb5693414687566c06132fed4a3
Sviatoslav <nhlsviatoslav@gmail.com>
2024-01-18 18:04:45 +0100

    fixed empty addings

--- announcment/post.php
+++ announcment/post.php
@@ -1,19 +1,21 @@
 <?php
     $title = filter_input(INPUT_POST, "title");
     $description = filter_input(INPUT_POST, "text");
-    try {
-        $query = $db->prepare(
-            "INSERT INTO announcments(title, description, date, author)
-        VALUES (:title, :description, :date, :author)"
-        );
-        $query->execute([
-            "title" => "$title",
-            'description' => "$description",
-            "date" => date("m-d-y"),
-            "author" => "author"
-        ]);
-        echo "announcment was sent";
-    } catch (PDOException $ex) {
-        echo "Announcment was not sent";
+    if ($title && $description) {
+        try {
+            $query = $db->prepare(
+                "INSERT INTO announcments(title, description, date, author)
+                VALUES (:title, :description, :date, :author)"
+            );
+            $query->execute([
+                "title" => "$title",
+                'description' => "$description",
+                "date" => date("m-d-y"),
+                "author" => "author"
+            ]);
+            echo "announcment was sent";
+        } catch (PDOException $ex) {
+            echo "Announcment was not sent";
+        }
     }
 ?>
